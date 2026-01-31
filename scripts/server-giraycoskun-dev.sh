726326a803ef3081ae2dd4d4b30f3a4dfc2d8d77
giraycoskun <giraycoskun.dev@gmail.com>
2026-01-31 17:32:51 +0100

    fix: refactor script to use defined command variables for git and pnpm

--- scripts/server-giraycoskun-dev.sh
+++ scripts/server-giraycoskun-dev.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 
+# Define paths explicitly
+GIT_CMD="/usr/bin/git"    # Update this based on 'which git' output
+PNPM_CMD="/usr/local/bin/pnpm" # Update this based on 'which pnpm' output
+
 cd /home/giraycoskun/Code/server.giraycoskun.dev
 
 echo "--- Fetching latest changes ---"
-git fetch https://${GITHUB_TOKEN}@github.com/giraycoskun/server.giraycoskun.dev.git main
+$GIT_CMD fetch https://${GITHUB_TOKEN}@github.com/giraycoskun/server.giraycoskun.dev.git main
 
 echo "--- Pulling latest changes ---"
-git pull https://${GITHUB_TOKEN}@github.com/giraycoskun/server.giraycoskun.dev.git main
+$GIT_CMD pull https://${GITHUB_TOKEN}@github.com/giraycoskun/server.giraycoskun.dev.git main
 
 echo "--- Installing deps ---"
-pnpm install
+$PNPM_CMD install
 
 echo "--- Building project with local ---"
-pnpm build:local
+$PNPM_CMD build:local
 
 echo "--- Building project with external ---"
-pnpm build:external
+$PNPM_CMD build:external
 
 echo "--- Server is Updated ---"
