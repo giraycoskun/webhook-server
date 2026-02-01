191f2d25a694f05c582d096d35582789dab296c7
giraycoskun <giraycoskun.dev@gmail.com>
2026-02-01 14:17:17 +0100

    fix: add logging into GitHub Container Registry in f1-board script

--- scripts/f1-board.sh
+++ scripts/f1-board.sh
@@ -17,6 +17,9 @@ git fetch https://${GITHUB_TOKEN}@github.com/giraycoskun/f1-board.git main
 echo "--- Pulling latest changes ---"
 git pull https://${GITHUB_TOKEN}@github.com/giraycoskun/f1-board.git main
 
+echo "--- Logging into GitHub Container Registry ---"
+echo "$GITHUB_TOKEN" | docker login ghcr.io -u giraycoskun --password-stdin
+
 echo "--- Stopping existing Docker containers ---"
 docker-compose -f $DOCKER_COMPOSE_FILENAME down
 
