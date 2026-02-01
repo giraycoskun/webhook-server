8dee011cd2d94632f8962fe889cbc56a7b8e3d4f
giraycoskun <giraycoskun.dev@gmail.com>
2026-02-01 12:50:57 +0100

    fix: f1-board script ensure project directory exists and clone repository if missing

--- scripts/f1-board.sh
+++ scripts/f1-board.sh
@@ -2,11 +2,25 @@
 set -e  # Exit immediately if any command fails
 
 DOCKER_COMPOSE_FILENAME="docker-compose.prod.yml"
+PROJECT_DIR="/home/giraycoskun/Code/f1-board"
+GIT_REPO="https://github.com/giraycoskun/f1-board.git"
 
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
+echo "--- Ensuring project directory exists ---"
+if [ ! -d "$PROJECT_DIR" ]; then
+    echo "Directory does not exist. Creating and cloning repository..."
+    mkdir -p "$PROJECT_DIR"
+    git clone "https://${GITHUB_TOKEN}@github.com/giraycoskun/f1-board.git" "$PROJECT_DIR"
+else
+    echo "Directory exists."
+fi
+
 echo "--- Changing directory to f1-board ---"
-cd /home/giraycoskun/Code/f1-board
+cd "$PROJECT_DIR"
+
+echo "--- Pulling latest changes from Git ---"
+git pull "https://${GITHUB_TOKEN}@github.com/${GIT_REPO_NAME}.git" main
 
 echo "--- Stopping existing Docker containers ---"
 docker-compose -f $DOCKER_COMPOSE_FILENAME down
